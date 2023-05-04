0be48b36b717c16364683f5805217bb284f45100
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-05-04 19:35:08 +0200

    added missing routing and cleaned up

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ require 'Routing.php';
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
-Router::get('login', 'DefaultController');
-Router::get('main', 'DefaultController');
+Router::set('login', 'DefaultController');
+Router::set('main', 'DefaultController');
+Router::set('product', 'DefaultController');
+Router::set('profile', 'DefaultController');
+Router::set('', 'DefaultController');
 Router::run($path);
