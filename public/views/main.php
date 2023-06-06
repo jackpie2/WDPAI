52c5a30e9133df08d03ba6a65be4b51dd1191883
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-06-06 21:42:16 +0200

    minor styling tweaks

--- public/views/main.php
+++ public/views/main.php
@@ -88,12 +88,8 @@
             <div class="coffee-content">
                 <div class="coffee-title">Untitled</div>
                 <div class="coffee-brand">Unknown brand</div>
-                <div class="coffee-rating"><span class="coffee-rating-number">0</span>
-                    <div class="star filled">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-                            <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
-                        </svg>
-                    </div>
+                <div class="coffee-rating">
+
                 </div>
                 <div class="coffee-description"></div>
             </div>
