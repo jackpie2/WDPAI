bbb0d1b96e1818475c0991082f722d2c6393781b
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-06-06 21:42:16 +0200

    minor styling tweaks

--- public/js/search.js
+++ public/js/search.js
@@ -39,6 +39,29 @@ function loadCoffee(coffee) {
     });
 }
 
+function makeStars(rating) {
+    let stars = "";
+    for (let i = 0; i < rating; i++) {
+        stars += `
+        <div class="star filled">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+                <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
+            </svg>
+        </div>
+        `;
+    }
+    for (let i = rating; i < 5; i++) {
+        stars += `
+        <div class="star">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+        <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
+        </svg>
+        </div>`;
+    }
+
+    return stars;
+}
+
 function createCoffee(coffee) {
     const template = document.getElementById("coffee-template");
 
@@ -58,8 +81,12 @@ function createCoffee(coffee) {
         ? coffee.brand_name
         : "Unknown brand";
 
-    const coffeeRatingNumber = clone.querySelector(".coffee-rating-number");
-    coffeeRatingNumber.innerHTML = (coffee.rating ? coffee.rating : 0) + "/5";
+    const coffeeRating = clone.querySelector(".coffee-rating");
+    coffeeRating.innerHTML =
+        makeStars(coffee.rating) +
+        " " +
+        (coffee.rating ? coffee.rating : 0) +
+        "/5";
 
     const productLink = clone.querySelector(".product-link");
     productLink.href = "product?id=" + coffee.id;
