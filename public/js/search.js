506b324fe1f115ca3ebb47bce9e9d7cc8096da11
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-06-08 00:07:54 +0200

    added loading display

--- public/js/search.js
+++ public/js/search.js
@@ -1,98 +1,107 @@
 function fetchData(page) {
-    let searchInput = document.getElementById("search-input");
-    let query = searchInput.value;
+	let searchInput = document.getElementById("search-input");
+	let query = searchInput.value;
 
-    fetch(`/search?q=${query}&page=${page}`, {
-        method: "GET",
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            loadCoffee(data);
-            updateLastPage(data.totalPages);
-            updateButtons();
-        });
+	fetch(`/search?q=${query}&page=${page}`, {
+		method: "GET",
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			loadCoffee(data);
+			updateLastPage(data.totalPages);
+			updateButtons();
+			disableLoading();
+		});
+}
+
+function enableLoading() {
+	const loading = document.getElementById("loading");
+	loading.style.display = "flex";
+}
+
+function disableLoading() {
+	const loading = document.getElementById("loading");
+	loading.style.display = "none";
 }
 
 function search() {
-    resetCurrentPage();
-    fetchData(1);
+	resetCurrentPage();
+	fetchData(1);
 }
 
 function loadCoffee(coffee) {
-    coffeeList = document.getElementsByClassName("coffee-list");
-    coffeeList[0].innerHTML = "";
+	let coffeeList = document.getElementsByClassName("coffee-list");
+	coffeeList[0].innerHTML = "";
 
-    currentPage = coffee.page;
-    maxPage = coffee.totalPages;
+	currentPage = coffee.page;
+	let maxPage = coffee.totalPages;
 
-    if (maxPage == 0) {
-        const div = document.createElement("div");
-        div.classList.add("no-results-box");
-        const text = document.createTextNode("No results found.");
-        div.appendChild(text);
-        coffeeList[0].appendChild(div);
-        return;
-    }
+	if (maxPage == 0) {
+		const div = document.createElement("div");
+		div.classList.add("no-results-box");
+		const text = document.createTextNode("No results found.");
+		div.appendChild(text);
+		coffeeList[0].appendChild(div);
+		return;
+	}
 
-    coffee.coffee.forEach((element) => {
-        createCoffee(element);
-    });
+	coffee.coffee.forEach((element) => {
+		createCoffee(element);
+	});
 }
 
 function makeStars(rating) {
-    let stars = "";
-    for (let i = 0; i < rating; i++) {
-        stars += `
+	let stars = "";
+	for (let i = 0; i < rating; i++) {
+		stars += `
         <div class="star filled">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
                 <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
             </svg>
         </div>
         `;
-    }
-    for (let i = rating; i < 5; i++) {
-        stars += `
+	}
+	for (let i = rating; i < 5; i++) {
+		stars += `
         <div class="star">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
         <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
         </svg>
         </div>`;
-    }
+	}
 
-    return stars;
+	return stars;
 }
 
 function createCoffee(coffee) {
-    const template = document.getElementById("coffee-template");
+	const template = document.getElementById("coffee-template");
 
-    const clone = template.content.cloneNode(true);
+	const clone = template.content.cloneNode(true);
 
-    const image = clone.querySelector("img");
-    image.src = "public/uploads/" + coffee.image_file;
+	const image = clone.querySelector("img");
+	image.src = "public/uploads/" + coffee.image_file;
 
-    const coffeeTitle = clone.querySelector(".coffee-title");
-    coffeeTitle.innerHTML = coffee.name ? coffee.name : "Unknown coffee";
+	const coffeeTitle = clone.querySelector(".coffee-title");
+	coffeeTitle.innerHTML = coffee.name ? coffee.name : "Unknown coffee";
 
-    const coffeeDescription = clone.querySelector(".coffee-description");
-    coffeeDescription.innerHTML = coffee.description ? coffee.description : "";
+	const coffeeDescription = clone.querySelector(".coffee-description");
+	coffeeDescription.innerHTML = coffee.description ? coffee.description : "";
 
-    const coffeeBrand = clone.querySelector(".coffee-brand");
-    coffeeBrand.innerHTML = coffee.brand_name
-        ? coffee.brand_name
-        : "Unknown brand";
+	const coffeeBrand = clone.querySelector(".coffee-brand");
+	coffeeBrand.innerHTML = coffee.brand_name
+		? coffee.brand_name
+		: "Unknown brand";
 
-    const coffeeRating = clone.querySelector(".coffee-rating");
-    coffeeRating.innerHTML =
-        makeStars(coffee.rating) +
-        " " +
-        (coffee.rating ? coffee.rating : 0) +
-        "/5";
+	const coffeeRating = clone.querySelector(".coffee-rating");
+	coffeeRating.innerHTML =
+		makeStars(coffee.rating) + " " + (coffee.rating ? coffee.rating : 0) + "/5";
 
-    const productLink = clone.querySelector(".product-link");
-    productLink.href = "product?id=" + coffee.id;
+	const productLink = clone.querySelector(".product-link");
+	productLink.href = "product?id=" + coffee.id;
 
-    coffeeList = document.getElementsByClassName("coffee-list");
-    coffeeList[0].appendChild(clone);
+	coffeeList = document.getElementsByClassName("coffee-list");
+	coffeeList[0].appendChild(clone);
 }
 
+enableLoading();
 search();
