2188d6ef73f0582615d0d861d50c9c6a0c10e8f1
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-06-22 00:13:45 +0200

    added basic footer

---
+++ public/templates/footer.php
@@ -0,0 +1,13 @@
+<?php
+
+echo <<<NAVBAR
+    <footer>
+        <h3>BrewReview</h3>
+        <div class="footer-links">
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/privacy">Privacy</a>
+            <a href="/terms">Terms</a>
+        </div>
+    </footer>
+NAVBAR;
