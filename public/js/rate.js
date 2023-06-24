a339fb72111aff6249a07862e7f46d9f28653191
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-06-24 17:42:11 +0200

    modified rating logic

--- public/js/rate.js
+++ public/js/rate.js
@@ -1,42 +1,56 @@
 function rate(rating) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const coffeeID = urlParams.get("id");
+    const urlParams = new URLSearchParams(window.location.search);
+    const coffeeID = urlParams.get("id");
 
-  fetch("/rateProduct", {
-    credentials: "include",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      coffeeID: coffeeID,
-      rating: rating,
-    }),
-  }).then(function (response) {
-    console.log(response);
-  });
+    fetch("/rateProduct", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            coffeeID: coffeeID,
+            rating: rating,
+        }),
+    }).then(function (response) {
+        console.log(response);
+    });
 
-  updateStars(rating);
+    updateStars(rating);
 }
 
 function updateStars(rating) {
-  const stars = document.querySelectorAll(".stars svg");
+    const stars = document.querySelectorAll(".stars svg");
+    const filledStarsCount = document.querySelectorAll(
+        "div.stars > svg.filled"
+    ).length;
+    const starsMobile = document.querySelectorAll(".stars-mobile svg");
 
-  for (let i = 0; i < stars.length; i++) {
-    if (i < rating) {
-      stars[i].classList.add("filled");
-    } else {
-      stars[i].classList.remove("filled");
+    console.log(document.querySelectorAll("div.stars svg.filled"));
+
+    if (filledStarsCount / 2 == rating) {
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].classList.remove("filled");
+        }
+        for (let i = 0; i < stars.length; i++) {
+            starsMobile[i].classList.remove("filled");
+        }
+        return;
     }
-  }
 
-  const starsMobile = document.querySelectorAll(".stars-mobile svg");
-
-  for (let i = 0; i < starsMobile.length; i++) {
-    if (i < rating) {
-      starsMobile[i].classList.add("filled");
-    } else {
-      starsMobile[i].classList.remove("filled");
+    for (let i = 0; i < stars.length; i++) {
+        if (i < rating) {
+            stars[i].classList.add("filled");
+        } else {
+            stars[i].classList.remove("filled");
+        }
     }
-  }
+
+    for (let i = 0; i < starsMobile.length; i++) {
+        if (i < rating) {
+            starsMobile[i].classList.add("filled");
+        } else {
+            starsMobile[i].classList.remove("filled");
+        }
+    }
 }
